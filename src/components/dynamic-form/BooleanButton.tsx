da1052233e1addde0f151519d92aac398eb182c6
nr <nejcr@yahoo.com>
2021-08-23 19:07:36 +0200

    apply formatting

--- src/components/dynamic-form/BooleanButton.tsx
+++ src/components/dynamic-form/BooleanButton.tsx
@@ -1,7 +1,7 @@
-import { FormFieldRendererProps } from './FormField';
-import { StyledBooleanButton } from '../styled/StyledButton';
+import {FormFieldRendererProps} from './FormField';
+import {StyledBooleanButton} from '../styled/StyledButton';
 import styled from 'styled-components';
-import { StyledFlex } from '../styled/StyledFlex';
+import {StyledFlex} from '../styled/StyledFlex';
 
 const StyleHiddenRadio = styled.input`
   float: right;
@@ -11,36 +11,36 @@ const StyleHiddenRadio = styled.input`
   opacity: 0;
 `;
 export const BooleanButton = (
-  props: Pick<FormFieldRendererProps, 'value' | 'onChange' | 'metadata'>,
+    props: Pick<FormFieldRendererProps, 'value' | 'onChange' | 'metadata'>,
 ) => {
-  const { onChange, metadata, value } = props;
+  const {onChange, metadata, value} = props;
 
   return (
-    <div>
-      <StyleHiddenRadio {...metadata} value={value} readOnly />
-      <StyledFlex>
-        <StyledBooleanButton
-          type={'button'}
-          isChecked={value === 'true'}
-          onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-            onChange('true')
-          }
-          borderRadius={'10px 0px 0px 10px'}
-        >
-          Yes
-        </StyledBooleanButton>
-        &nbsp;
-        <StyledBooleanButton
-          type={'button'}
-          isChecked={value === 'false'}
-          onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-            onChange('false')
-          }
-          borderRadius={'0px 10px 10px 0px'}
-        >
-          No
-        </StyledBooleanButton>
-      </StyledFlex>
-    </div>
+      <div>
+        <StyleHiddenRadio {...metadata} value={value} readOnly/>
+        <StyledFlex>
+          <StyledBooleanButton
+              type={'button'}
+              isChecked={value === 'true'}
+              onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+                  onChange('true')
+              }
+              borderRadius={'10px 0px 0px 10px'}
+          >
+            Yes
+          </StyledBooleanButton>
+          &nbsp;
+          <StyledBooleanButton
+              type={'button'}
+              isChecked={value === 'false'}
+              onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+                  onChange('false')
+              }
+              borderRadius={'0px 10px 10px 0px'}
+          >
+            No
+          </StyledBooleanButton>
+        </StyledFlex>
+      </div>
   );
 };
